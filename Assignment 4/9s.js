3ed51bbbc4679438e35cbe858d1d847fa1202082
simo-grande <smerhay@miu.edu>
2020-08-24 13:47:48 -0500

    Update 9s.js
    update on no 9

--- Assignment 4/9s.js
+++ Assignment 4/9s.js
@@ -1,22 +1,7 @@
 let prompt = require('prompt-sync')();
 let n= prompt('Enter a number: ');
-for(let i = n; i<=n; i++){
-    let row="";
-     for(let j=1; j<=n; j++){
-        if(j===1){
-            row+=n+"\t" ;
-        }
-         else if(j===2){
-             row+=2*n+"\t" ;
-         } else if(j===3){
-             row+=3*n+"\t" ;
-         }else if(j===4){
-            row+=n*n+"\t" ;
-         }else if(j===5){
-            row+=n*n*n+"\t" ;
-         }
-      
-        
-    }
+    n= parseInt(n);
+for(let i = 1; i<=n; i++){
+    let row=i+" "+2*i+" "+3*i+" "+i*i+" "+i*i*i;
     console.log(row);
-    }
+}
